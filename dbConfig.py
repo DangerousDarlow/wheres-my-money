e729018460030ae061fc061ff8a3b19718e935ac
Nick Darlow <dangerous.darlow@gmail.com>
2021-01-18 23:44:57 +0000

    Linted all python files

--- dbConfig.py
+++ dbConfig.py
@@ -1,11 +1,13 @@
 from psycopg2.extras import register_uuid
 
+
 class DbConfig:
     dbName = 'money'
     dbUser = 'postgres'
     dbPassword = 'STy6KEnk'
 
-# required to read and write uuid type to postgres using psycopg2  
+
+# required to read and write uuid type to postgres using psycopg2
 register_uuid()
 
 dbConfig = DbConfig()
--- transaction.py
+++ transaction.py
@@ -1,11 +1,13 @@
 from datetime import date
 from uuid import UUID, uuid4
 
+
 class Transaction:
-    def __init__(self, id = uuid4(), date = date.today(), amount = 0, description = ''):
+    def __init__(self, id=uuid4(), date=date.today(), amount=0, description=''):
         self.id = id
         self.date = date
         self.amount = amount
         self.description = description
+
 
 AmountScalingFactor = 1000000
