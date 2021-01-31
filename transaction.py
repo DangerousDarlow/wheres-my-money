65dec80fca9aa79f796a8dd0862b8742ac5f8f75
Nick Darlow <dangerous.darlow@gmail.com>
2021-01-31 17:32:28 +0000

    Refactor get untagged transactions to return Transaction object

--- transaction.py
+++ transaction.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from decimal import Decimal
 from uuid import UUID, uuid4
 
 
@@ -10,6 +11,11 @@ class Transaction:
         self.description = description
         self.added = added
         self.account = account
+
+    def to_string(self):
+        amount = Decimal(self.amount) / AmountScalingFactor
+        amountStr = '{0:.2f}'.format(amount)
+        return '{:<}   {:<}   {:>8}   {:<}'.format(str(self.id), str(self.timestamp), amountStr, self.description)
 
 
 AmountScalingFactor = 1000000
--- untagged_transactions.py
+++ untagged_transactions.py
@@ -1,24 +1,31 @@
 from argparse import ArgumentParser
 from dbConfig import dbConnectionString
 from psycopg2 import connect
+from transaction import Transaction
+
+
+def getUntaggedTransactions(dbConnection, limit):
+    countTransactions = 'SELECT COUNT(id) FROM transactions t FULL JOIN transactions_tags tt ON t.id = tt.transaction_id WHERE tt.transaction_id IS NULL'
+    selectTransactions = 'SELECT id, timestamp, amount, description, added, account FROM transactions t FULL JOIN transactions_tags tt ON t.id = tt.transaction_id WHERE tt.transaction_id IS NULL ORDER BY t.timestamp DESC LIMIT %s'
+
+    with dbConnection.cursor() as dbCursor:
+        dbCursor.execute(countTransactions)
+        count = dbCursor.fetchone()[0]
+
+        dbCursor.execute(selectTransactions, [limit])
+        transactions = [Transaction(row[0], row[1], row[2], row[3], row[4], row[5]) for row in dbCursor.fetchall()]
+        return (count, transactions)
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='List transactions without tags')
     parser.add_argument('--limit', type=int, default=10, help='Maximum number of transactions returned')
     args = parser.parse_args()
 
-    countTransactions = 'SELECT COUNT(id) FROM transactions t FULL JOIN transactions_tags tt ON t.id = tt.transaction_id WHERE tt.transaction_id IS NULL'
-    selectTransactions = 'SELECT id, timestamp, description, readable(amount) FROM transactions t FULL JOIN transactions_tags tt ON t.id = tt.transaction_id WHERE tt.transaction_id IS NULL ORDER BY t.timestamp DESC LIMIT %s'
-
     with connect(dbConnectionString) as dbConnection:
-        with dbConnection.cursor() as dbCursor:
-            dbCursor.execute(countTransactions)
-            count = dbCursor.fetchone()
-            print(f'{count[0]} transactions without tags')
-            print()
+        (count, transactions) = getUntaggedTransactions(dbConnection, args.limit)
+        print(f'{count} transactions without tags')
+        print()
 
-            dbCursor.execute(selectTransactions, [args.limit])
-            transactions = dbCursor.fetchall()
-            for transaction in transactions:
-                amount = '{0:.2f}'.format(transaction[3])
-                print('{:<}   {:<}   {:<8}   {:<}'.format(str(transaction[0]), str(transaction[1]), amount, transaction[2]))
+        for transaction in transactions:
+            print(transaction.to_string())
